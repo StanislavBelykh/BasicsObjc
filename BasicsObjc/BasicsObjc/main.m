1038b51ff865be9c516c8cda534d7ac65fecc552
Stanislav Belykh <apple.stas@outlook.com>
2020-09-14 22:49:45 +0300

    HW Lesson 2

--- BasicsObjc/BasicsObjc/main.m
+++ BasicsObjc/BasicsObjc/main.m
@@ -8,52 +8,47 @@
 
 #import <Foundation/Foundation.h>
 
+void checkChar(char character) {
+    int intChar = character;
+    
+    if ((intChar >= 65 && intChar <= 90) || (intChar >= 97 && intChar <= 122)){
+        printf("Буква %c из английского алфавита \n", character);
+    } else {
+        printf("Cимвол %c не принадлежит английскому алфавиту \n", character);
+    }
+}
+
+enum OperationType {
+    plus,
+    minus,
+    divide,
+    multiply
+} OperationType;
+
+double mathFunc(enum OperationType operation, double first, double second) {
+    double result;
+    switch (operation) {
+        case plus:
+            result = first + second;
+            break;
+        case minus:
+            result = first - second;
+            break;
+        case divide:
+            result = first / second;
+            break;
+        case multiply:
+            result = first * second;
+            break;
+    }
+    return result;
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int first = 0;
-        int second = 0;
-
-        int operation = 0;
-
-        printf("First number: \n");
-        scanf("%d", &first);
-        printf("Second number: \n");
-        scanf("%d", &second);
-        printf("Select operation! \n Enter operation number, \n 1 for sum \n 2 for subtraction \n 3 for degree \n 4 for multiplication \n Number of operation: ");
-        scanf("%d", &operation);
-
-        switch (operation) {
-            case 1:
-                NSLog(@"Sum %d and %d: %d", first, second, first + second);
-                break;
-            case 2:
-                NSLog(@"Subtraction %d and %d: %d", first, second, first - second);
-                break;
-            case 3:
-                NSLog(@"%d divided by %d: %d", first, second, first / second);
-                break;
-            case 4:
-                NSLog(@"%d multiply %d: %d", first, second, first * second);
-                break;
-            default:
-                printf("Error operation");
-                break;
-        }
-        printf("\n");
-
-        double firstAvr = 0;
-        double secondAvr = 0;
-        double thirdAvr = 0;
         
-        printf("Enter three num \n");
-        printf("First number: \n");
-        scanf("%lf", &firstAvr);
-        printf("Second number: \n");
-        scanf("%lf", &secondAvr);
-        printf("Third number: \n");
-        scanf("%lf", &thirdAvr);
-        
-        NSLog(@"Avarage for numbers(%.2f, %.2f, %.2f): %.2f", firstAvr, secondAvr, thirdAvr, (firstAvr + secondAvr + thirdAvr)/3);
+        checkChar('A');
+        printf("Result: %0.2f \n", mathFunc(plus, 1, 1) );
     }
     return 0;
 }
